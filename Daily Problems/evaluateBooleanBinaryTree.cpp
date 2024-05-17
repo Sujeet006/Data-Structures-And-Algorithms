c09309c5cecca056bfd9fc50feabcbf5f9bc476c
Sujeet Singh <120650221+Sujeet006@users.noreply.github.com>
2024-05-17 05:44:47 +0000

    Day 5

---
+++ Daily Problems/evaluateBooleanBinaryTree.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool helper(TreeNode* root){
+    if(root->val==0 || root->val==1){
+        return root->val==1;
+    }
+
+    else if(root->val == 2){
+        return helper(root->left) || helper(root->right);
+    }
+    else{
+        return helper(root->left) && helper(root->right);
+    }
+
+    return false;
+}
+
+bool evaluateTree(TreeNode* root){
+    return helper(root);
+}
+
+void printTree(TreeNode* root) {
+    if (root == NULL) return;
+    printTree(root->left);
+    cout << root->val << " ";
+    printTree(root->right);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(0);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(1);
+    root->left->left = new TreeNode(3);
+    root->left->right = new TreeNode(1);
+
+    int target = 2;
+    cout << "Original tree (Inorder): ";
+    printTree(root);
+    cout << endl;
+
+    bool ans=evaluateTree(root);
+    cout<<ans<<endl;
+}
